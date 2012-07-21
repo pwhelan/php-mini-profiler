3238cf1c0011158d5b4cba1fb8b298d622fbdc4a
Phillip Whelan <pwhelan@mixxx.org>
2012-07-21 01:17:29 -0700

    FIX: correctly report on AJAX requests that occur before render.js is loaded.

--- js/render.js
+++ js/render.js
@@ -2,44 +2,68 @@
  * Render the tag with the total count for the profile statistics
  */
 
+var QueryAccordionOptions = {
+	active: false,
+	collapsible:true,
+	autoHeight: false
+};
+
+renderAjaxTemplate = function(tmpl, path, ajaxId) {
+	if (PhpMiniProfiler.AJAXRequestUrl) {
+		$.ajax({
+			url: PhpMiniProfiler.AJAXRequestUrl + '/' + ajaxId,
+			dataType: 'json',
+			beforeSend: function(jqXHR) {
+				jqXHR.setRequestHeader('X-Php-Mini-Profiler', '1');
+			},
+			success: function(data){
+				data.path = path;
+				
+				$('#pmp-profiler-details').append(
+					Mustache.render(ajaxTemplate, data)
+				);
+				$('.queries').last().accordion(QueryAccordionOptions);
+			}
+		});
+	}
+};
+
+renderTemplate = function(tmplName) {
+	$.ajaxSetup({async:false});
+	$.get(PhpMiniProfiler.includePath + '/templates/' + tmplName, {}, 
+		function(tmpl) {
+			console.log('TEMPLATE HAS LOADED, RENDER NOW');
+			$('BODY').prepend(
+				Mustache.render(tmpl, PhpMiniProfiler)
+			);
+		},
+		'html'
+	);
+	$.ajaxSetup({async:false});
+};
+
 // TODO: change cursor to hand!
-$(window).ready(function() {
+//$(window).ready(function() {
 	
 	if ( typeof window.PhpMiniProfiler == 'object') {
+		console.log('RENDERING NOW...');
+		
 		var details = $('#pmp-profiler-details');
-		
-		
-		var renderTemplate = function(tmplName) {
-			$.ajaxSetup({async: false});
-			$.get(PhpMiniProfiler.includePath + '/templates/' + tmplName, {}, 
-				function(tmpl) {
-					$('BODY').prepend(
-						Mustache.render(tmpl, PhpMiniProfiler)
-					);
-				},
-				'html'
-			);
-			$.ajaxSetup({async: true});
-		};
 		
 		renderTemplate('floater.ms');
 		renderTemplate('details.ms');
 		
-		$('#pmp-profiler-details .showqueries').click(function() {
-			var shown = $('#pmp-profiler-details .queries').css('display') != 'none';
+		$('#pmp-profiler-details .showqueries').live('click', function() {
+			var queries = $(this).siblings('.queries');
+			var shown = queries.css('display') != 'none';
+			
 			$(this).html(shown ? 'Show Queries' : 'Hide Queries');
-			$('#pmp-profiler-details .queries').css('display', 
-				shown ? 'none' : 'block'
-			);
+			queries.css('display', shown ? 'none' : 'block');
 		});
 		
 		if (typeof $.ui.accordion == 'function') {
 			$('#pmp-profiler-details .queries').
-				accordion({
-					active: false,
-					collapsible:true,
-					autoHeight: false
-				});
+				accordion(QueryAccordionOptions);
 		}
 		
 		var displayedDetails = false;
@@ -58,5 +82,4 @@ $(window).ready(function() {
 		
 	}
 	
-});
-
+//});
